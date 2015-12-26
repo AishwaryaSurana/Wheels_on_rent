3c201ee4de6a5cd7681466ccdbe138863b8571a2
aishwaryasurana <aishwaryasurana.278@gmail.com>
2015-12-26 14:16:00 +0530

    registration

---
+++ Rental2/app/src/androidTest/java/com/aish/aishwarya/rental/WebHelper.java
@@ -0,0 +1,13 @@
+package com.aish.aishwarya.rental;
+
+/**
+ * Created by Aishwarya on 24-Dec-15.
+ */
+public class WebHelper
+{
+    public static final String baseUrl=" 192.168.56.1";
+    public static final String dbname="wheels_on_rent";
+    public static final String userName="root";
+    public static final String password="";
+
+}
