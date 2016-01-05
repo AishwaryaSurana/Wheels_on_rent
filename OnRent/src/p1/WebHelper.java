9e54528e8242ee8c94442b3b2424cc02fc9eb6be
aishwarya <aishwaryasurana.278@gmail.com>
2016-01-05 18:09:51 +0530

    Login and Registration

---
+++ OnRent/src/p1/WebHelper.java
@@ -0,0 +1,13 @@
+package p1;
+
+/**
+ * Created by Aishwarya on 24-Dec-15.
+ */
+public class WebHelper
+{
+    public static final String baseUrl="http://192.168.1.7:8084";
+    public static final String dbname="wheels_on_rent";
+    public static final String userName="root";
+    public static final String password="root";
+    //F:\netbeans_project\WheelsOnRentWebApp\src\java\pkg\LoginServlet.java
+}
